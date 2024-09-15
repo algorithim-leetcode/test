a4c945bee67580e16a5a9041552615567688571d
radiant1010 <doomsday123@naver.com>
2024-09-15 14:35:11 +0900

    MySQL 설정 변경

--- src/main/java/com/toy/truffle/TruffleApplication.java
+++ src/main/java/com/toy/truffle/TruffleApplication.java
@@ -7,15 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TruffleApplication {
     public static void main(String[] args) {
-        // .env 파일 로딩
-        Dotenv dotenv = Dotenv.load();
-
-        // 데이터 베이스 연결 설정
-        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-        System.setProperty("spring.datasource.driver-class-name", dotenv.get("DB_DRIVER"));
-
         SpringApplication.run(TruffleApplication.class, args);
     }
 
