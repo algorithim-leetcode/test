b6a53781bcd4e579252d5119d84b7e900aa91522
radiant1010 <doomsday123@naver.com>
2024-09-22 15:07:06 +0900

    테스트 코드 리스트 작성

---
+++ src/test/java/com/toy/truffle/user/UserSignUpTest.java
@@ -0,0 +1,37 @@
+package com.toy.truffle.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserSignUpTest {
+
+	@Test
+	@DisplayName("회원정보 저장")
+	public void testSaveUserSignUp() {
+		// given
+
+		// when
+
+		// then
+	}
+
+	@Test
+	@DisplayName("비밀번호 암호화")
+	public void testEncodePassword() {
+		// given
+
+		// when
+
+		// then
+	}
+
+	@Test
+	@DisplayName("입력 값 정규식 체크")
+	public void testCheckValidation() {
+		// given
+
+		// when
+
+		// then
+	}
+}
